e9cb1ef4b54cee1ab779b8d991364fd67e4ffdee
Jan Fooken <git@bode.fun>
2023-08-16 15:55:57 +0200

    Experiment with config files.
    Currently yaml is more readable and I like it

--- main.go
+++ main.go
@@ -3,17 +3,52 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/charmbracelet/log"
+	"gopkg.in/yaml.v3"
 )
 
+var Name = "adsig" //nolint
+
 func main() {
-	if err := mainE(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	log := log.NewWithOptions(os.Stderr, log.Options{ //nolint
+		Prefix:          Name,
+		ReportTimestamp: true,
+		ReportCaller:    true,
+	})
+
+	if err := mainE(log); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func mainE() error {
-	fmt.Println("Hello, World") //nolint
+func mainE(log *log.Logger) error {
+	cnf, err := os.Open("adsig.yml")
+	if err != nil {
+		return err
+	}
+	defer cnf.Close()
+
+	var v struct {
+		Connection struct {
+			User     string
+			Password string `yaml:",omitempty"`
+		}
+		Template map[string]struct {
+			AdFilter      string            `yaml:"adFilter,omitempty"`
+			BaseDN        string            `yaml:"baseDN"`
+			ExcludeEmails []string          `yaml:"excludeEmails,omitempty"`
+			FieldMapping  map[string]string `yaml:"fieldMapping"` // TODO: Maybe I will need to case the index? https://stackoverflow.com/questions/75535015/go-to-unmarshal-into-uppercase-keys
+		}
+	}
+
+	ymlDecoder := yaml.NewDecoder(cnf)
+	ymlDecoder.KnownFields(false)
+	if err := ymlDecoder.Decode(&v); err != nil {
+		return err
+	}
+
+	fmt.Printf("%+v", v)
 
 	return nil
 }
