05a7c0c0b14403ed7b86823fa4554bd1f92568ba
Jan Fooken <git@bode.fun>
2023-08-17 15:03:40 +0200

    Extract config into own package and change keys
    Change connection.user to connection.userDN

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"io"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Type Definition
+// ------------------------------------------------------------------------
+
+type Connection struct {
+	UserDN   string `yaml:"userDN"`   //nolint
+	Password string `yaml:"password"` // TODO: Make this optional
+}
+
+type Group struct {
+	AdFilter      string   `yaml:"adFilter"`      // TODO: Make this optional
+	BaseDN        string   `yaml:"baseDN"`        //nolint
+	ExcludeEmails []string `yaml:"excludeEmails"` // TODO: Make this optional
+	Templates     []string
+}
+
+type Template struct {
+	// TODO: Maybe I will need to case the keys? https://stackoverflow.com/questions/75535015/go-to-unmarshal-into-uppercase-keys
+	FieldMapping map[string]string `yaml:"fieldMapping"`
+}
+
+type Config struct {
+	Connection Connection
+	Groups     map[string]Group
+	Templates  map[string]Template
+}
+
+// Public Functions
+// ------------------------------------------------------------------------
+
+// TODO: Validate and set defaults
+func FromYAML(r io.Reader) (Config, error) {
+	var cnf Config
+
+	decoder := yaml.NewDecoder(r)
+	decoder.KnownFields(true)
+
+	err := decoder.Decode(&cnf)
+
+	return cnf, err
+}
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+	"git.bode.fun/adsig/config"
 	"github.com/charmbracelet/log"
-	"gopkg.in/yaml.v3"
 )
 
 var Name = "adsig" //nolint
@@ -23,35 +22,18 @@ func main() {
 }
 
 func mainE(log *log.Logger) error {
-	cnf, err := os.Open("adsig.yml")
+	cnfFile, err := os.Open("adsig.yml")
 	if err != nil {
 		return err
 	}
-	defer cnf.Close()
+	defer cnfFile.Close()
 
-	var v struct {
-		Connection struct {
-			User     string
-			Password string `yaml:",omitempty"`
-		}
-		Groups map[string]struct {
-			AdFilter      string   `yaml:"adFilter,omitempty"`
-			BaseDN        string   `yaml:"baseDN"`
-			ExcludeEmails []string `yaml:"excludeEmails,omitempty"`
-			Templates     []string
-		}
-		Templates map[string]struct {
-			FieldMapping map[string]string `yaml:"fieldMapping"` // TODO: Maybe I will need to case the index? https://stackoverflow.com/questions/75535015/go-to-unmarshal-into-uppercase-keys
-		}
-	}
-
-	ymlDecoder := yaml.NewDecoder(cnf)
-	ymlDecoder.KnownFields(false)
-	if err := ymlDecoder.Decode(&v); err != nil {
+	cnf, err := config.FromYAML(cnfFile)
+	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%+v", v)
+	log.Infof("%#v", cnf)
 
 	return nil
 }
