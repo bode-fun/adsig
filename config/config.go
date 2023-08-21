2bd91661652e384018319436b48f8b30f83d08b4
Jan Fooken <git@bode.fun>
2023-08-21 12:31:42 +0200

    Add errors and server-config to config pkg

--- config/config.go
+++ config/config.go
@@ -1,48 +1,76 @@
 package config
 
 import (
+	"errors"
 	"io"
 
 	"gopkg.in/yaml.v3"
 )
 
+// Errors
+// ------------------------------------------------------------------------
+
+// TODO: Make a custom error type to wrap in.
+
+var (
+	ErrDecode    = errors.New("config: Can not content of decode config")
+	ErrNoContent = errors.New("config: The content file holds no content")
+)
+
 // Type Definition
 // ------------------------------------------------------------------------
 
+type Server struct {
+	Host         string `yaml:"host"`
+	Port         int    `yaml:"port"`
+	ReadTimeout  int    `yaml:"readTimeout"`
+	WriteTimeout int    `yaml:"writeTimeout"`
+}
+
 type Connection struct {
 	UserDN   string `yaml:"userDN"`   //nolint
-	Password string `yaml:"password"` // TODO: Make this optional
+	Password string `yaml:"password"` // TODO: Make this optional.
 }
 
 type Group struct {
-	AdFilter      string   `yaml:"adFilter"`      // TODO: Make this optional
+	AdFilter      string   `yaml:"adFilter"`      // TODO: Make this optional.
 	BaseDN        string   `yaml:"baseDN"`        //nolint
-	ExcludeEmails []string `yaml:"excludeEmails"` // TODO: Make this optional
+	ExcludeEmails []string `yaml:"excludeEmails"` // TODO: Make this optional.
 	Templates     []string
 }
 
 type Template struct {
-	// TODO: Maybe I will need to case the keys? https://stackoverflow.com/questions/75535015/go-to-unmarshal-into-uppercase-keys
 	Fields map[string]string `yaml:"fields"`
 }
 
 type Config struct {
-	Connection Connection
-	Groups     map[string]Group
-	Templates  map[string]Template
+	Server     Server              `yaml:"server"`
+	Connection Connection          `yaml:"connection"`
+	Groups     map[string]Group    `yaml:"groups"`
+	Templates  map[string]Template `yaml:"templates"`
 }
 
 // Public Functions
 // ------------------------------------------------------------------------
 
-// TODO: Validate and set defaults
+// TODO: Validate and set defaults.
 func FromYAML(r io.Reader) (Config, error) {
 	var cnf Config
 
 	decoder := yaml.NewDecoder(r)
 	decoder.KnownFields(true)
 
-	err := decoder.Decode(&cnf)
+	// err can be io.EOF or yaml.TypeError
+	if err := decoder.Decode(&cnf); err != nil {
+		if errors.Is(err, &yaml.TypeError{}) { //nolint
+			// TODO: Wrap the error.
+			return Config{}, ErrDecode
+		} else if errors.Is(err, io.EOF) {
+			return Config{}, ErrNoContent
+		} else {
+			return Config{}, ErrDecode
+		}
+	}
 
-	return cnf, err
+	return cnf, nil
 }
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"git.bode.fun/adsig/config"
+	"git.bode.fun/adsig/server"
 	"github.com/charmbracelet/log"
 )
 
@@ -33,7 +38,22 @@ func mainE(log *log.Logger) error {
 		return err
 	}
 
-	log.Infof("%#v", cnf)
+	addr := fmt.Sprintf("%s:%d", cnf.Server.Host, cnf.Server.Port)
+	handler := server.New()
+
+	// TODO: find a good value for the timeouts.
+	// Fixes gosec issue G114
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      handler,
+		ReadTimeout:  time.Duration(cnf.Server.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(cnf.Server.WriteTimeout) * time.Second,
+	}
+
+	log.Infof("Starting server on %s", addr)
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
 
 	return nil
 }
