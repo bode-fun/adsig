6b2277498d57413fc14cf88316f0d4233dc5615b
Jan Fooken <git@bode.fun>
2023-08-25 11:24:10 +0200

    Render templates into in memory KV store
    Delete them once they are queried

--- TODO.md
+++ TODO.md
@@ -8,8 +8,7 @@
 
 ## Templates
 
-- [ ] Store generated templates in the filesystem under `/var/lib/adsig`, because `/var/tmp` might be deleted. [Ref](https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch05s08.html)
-- [ ] Add a fs-lock to prevent that a file is written to and read from at the same time.
+- [ ] Store DB in the filesystem under `/var/lib/adsig`, because `/var/tmp` might be deleted. [Ref](https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch05s08.html)
 
 ## API
 
@@ -25,6 +24,4 @@
 ## Optimizations
 
 - [ ] Don't rebuild the Group/Member tree every request.
-- [ ] Use a cache or DB for the generated Signatures.
 - [ ] Try to reduce the references kept around in the Group/Member tree.
-- [ ] Store generated templates in an in-memory store
--- cmd/adsig/main.go
+++ cmd/adsig/main.go
@@ -6,14 +6,17 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"git.bode.fun/adsig"
 	"git.bode.fun/adsig/config"
-	"git.bode.fun/adsig/internal/util"
 	"git.bode.fun/adsig/server"
 	"github.com/charmbracelet/log"
+	"github.com/dgraph-io/badger/v3"
 	"github.com/go-ldap/ldap/v3"
+	"github.com/google/uuid"
 )
 
 var Name = "adsig" //nolint
@@ -43,8 +46,6 @@ func mainE(log *log.Logger) error {
 		return errors.New("email required but not provided")
 	}
 
-	accountSearch = util.NormalizeEmail(accountSearch)
-
 	cnfFile, err := os.Open("adsig.yml")
 	if err != nil {
 		return err
@@ -55,6 +56,12 @@ func mainE(log *log.Logger) error {
 	if err != nil {
 		return err
 	}
+
+	db, err := badger.Open(badger.DefaultOptions("").WithInMemory(true))
+	if err != nil {
+		return err
+	}
+	defer db.Close()
 
 	conn, err := ldap.DialURL(cnf.Connection.Address)
 	if err != nil {
@@ -71,9 +78,21 @@ func mainE(log *log.Logger) error {
 		return err
 	}
 
+	renderedTmplsPerMember := make(map[string][]struct {
+		ID   uuid.UUID
+		File string
+		Data string
+	}, 0)
+
 	for _, group := range groups {
 		if ok, member := group.MemberBySamAccountName(accountSearch); ok {
 			log.Infof("%s is a member of the group \"%s\" with %d members", accountSearch, group.Name, len(group.Members))
+
+			renderedTmplsPerMember[accountSearch] = make([]struct {
+				ID   uuid.UUID
+				File string
+				Data string
+			}, 0)
 
 			for _, sig := range group.Signatures {
 				tmpls, err := sig.ParseFiles()
@@ -81,25 +100,78 @@ func mainE(log *log.Logger) error {
 					return err
 				}
 
-				var v struct {
+				var tmplData struct {
 					Fields map[string]string
 				}
 
-				v.Fields = make(map[string]string)
+				tmplData.Fields = make(map[string]string)
 
 				for key, attribute := range sig.Fields {
-					v.Fields[key] = member.GetAttributeValue(attribute)
+					tmplData.Fields[key] = member.GetAttributeValue(attribute)
 				}
 
 				for _, tmpl := range tmpls {
-					fmt.Println()
-					fmt.Println(tmpl.Name())
-					fmt.Println("------------------------------------------------")
-					tmpl.Execute(os.Stdout, v)
+					var b strings.Builder
+
+					if err := tmpl.Execute(&b, tmplData); err != nil {
+						break
+					}
+
+					u := uuid.New()
+
+					ext := filepath.Ext(tmpl.Name())
+
+					renderedTmplsPerMember[accountSearch] = append(renderedTmplsPerMember[accountSearch], struct {
+						ID   uuid.UUID
+						File string
+						Data string
+					}{
+						ID:   u,
+						Data: b.String(),
+						File: sig.Name + ext,
+					})
 				}
 			}
 		}
 	}
+
+	for memberAccount, renderedTmpls := range renderedTmplsPerMember {
+		log.Infof("Printing tmpls for %s", memberAccount)
+
+		for _, tmpl := range renderedTmpls {
+			db.Update(func(txn *badger.Txn) error {
+				e := badger.NewEntry(tmpl.ID[:], []byte(tmpl.Data)).WithTTL(time.Hour)
+				err := txn.SetEntry(e)
+				return err
+			})
+
+			valCopy := make([]byte, 0)
+
+			db.View(func(txn *badger.Txn) error {
+				itm, err := txn.Get(tmpl.ID[:])
+				if err != nil {
+					return err
+				}
+
+				cpy, err := itm.ValueCopy(nil)
+				if err != nil {
+					return err
+				}
+
+				valCopy = append(valCopy, cpy...)
+
+				return nil
+			})
+
+			db.Update(func(txn *badger.Txn) error {
+				err := txn.Delete(tmpl.ID[:])
+				return err
+			})
+
+			log.Infof("name=%s val=%s", tmpl.File, string(valCopy))
+		}
+	}
+
 	return nil
 }
 
